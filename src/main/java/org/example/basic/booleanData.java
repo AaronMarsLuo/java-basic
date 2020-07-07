eb92658c0935851bd993f0de2868a71f154ee529
luomin <luomin@soul.app>
2020-07-07 17:58:00 +0800

    update

---
+++ src/main/java/org/example/basic/booleanData.java
@@ -0,0 +1,11 @@
+package org.example.basic;
+
+import org.junit.Test;
+
+public class booleanData {
+    @Test
+    public void test(){
+        Boolean is=null;
+        System.out.println(is==Boolean.FALSE);
+    }
+}
